0aa178d2b0eecb9e2ae6754e107ed37be9e19000
Simonenko75 <simonenkonikita00@gmail.com>
2023-02-28 15:18:40 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,3 @@
+tg_bot_token = "6030811426:AAFGUlR-bDfHX269Y3bEsDy4bNsFioGcqlw"
+App_URL = f"https://second-telebot-heroku.herokuapp.com/{tg_bot_token}"
+DB_URI = "postgres://pnoktlnbhlevzy:f741d057e848260f1b79321620bcdc25c8f4ddbc36593ead5cf1a678d87ffc16@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d2p030hru0dnqh"

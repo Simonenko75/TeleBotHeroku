0005edce179448ed5df2baff1be22b5f5ff26067
Simonenko75 <simonenkonikita00@gmail.com>
2023-02-28 16:15:49 +0200

    "next commit"

--- main.py
+++ main.py
@@ -36,6 +36,22 @@ def start(message):
     update_messages_count(user_id)
 
 
+@bot.message_handler(commands=["start"])
+def get_stars(message):
+    db_object.execute("SELECT * FROM users ORDER BY messages DESC LIMIT id")
+    result = db_object.fetchall()
+
+    if not result:
+        bot.reply_to(message, "No data...")
+    else:
+        reply_message = " - Top users:\n"
+        for i, item in enumerate(result):
+            reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} message.\n"
+        bot.reply_to(message, reply_message)
+
+    update_messages_count(message.from_user.id)
+
+
 @bot.message_handler(func=lambda message: True, content_types=["text"])
 def message_from_user(message):
     user_id = message.from_user.id

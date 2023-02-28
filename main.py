65e97d0a5aa4536d6abc3df0faef7d351cd2f4bd
Simonenko75 <simonenkonikita00@gmail.com>
2023-02-28 17:00:06 +0200

    "next commit"

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def get_stats(message):
     else:
         reply_message = " - Top users:\n"
         for i, item in enumerate(result):
-            reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2] + 1} messages.\n"
+            reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
         bot.reply_to(message, reply_message)
 
     update_messages_count(message.from_user.id)
